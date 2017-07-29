1e1f875d1f6d5267803db5efa2bf939d86d9a723
Abe Hiroshi <abe.hiroshi@monoplus.com>
2017-07-29 23:51:05 +0900

    Add line script

---
+++ myservice/line.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ -z "${SERVICE_URL}" ]; then
+  echo "env SERVICE_URL is not defined."
+  exit 1
+fi
+
+TEXT=$1
+
+if [ -z "$1" ]; then
+  echo "usage: $0 <text>"
+  exit 1
+fi
+
+curl -X POST "${SERVICE_URL}" -d - <<EOT
+{
+  "service_method": "line_push",
+  "text": "${TEXT}"
+}
+EOT
