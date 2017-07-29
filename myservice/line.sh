d7e2854af0307c621702e0e145f4aa9c82293d47
Abe Hiroshi <abe.hiroshi@monoplus.com>
2017-07-30 00:09:33 +0900

    Add to

--- myservice/line.sh
+++ myservice/line.sh
@@ -6,15 +6,17 @@ if [ -z "${SERVICE_URL}" ]; then
 fi
 
 TEXT=$1
+TO=${2-$LINE_TO_DEFAULT}
 
-if [ -z "$1" ]; then
-  echo "usage: $0 <text>"
+if [ -z "${TO}" ]; then
+  echo "usage: $0 <text> <to>"
   exit 1
 fi
 
 curl -X POST -H "Accept: application/json" -H "Content-type: application/json" "${SERVICE_URL}" -d @- <<EOT
 {
   "service_method": "line_push",
+  "to": "${TO}",
   "text": "${TEXT}"
 }
 EOT
